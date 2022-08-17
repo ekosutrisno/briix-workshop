6f1fcaa3a553ef86b900ec20eae31a67303f4628
ekosutrisno1 <ekosutrisno801@gmail.com>
2022-08-17 22:05:39 +0700

    update: add main class entry

---
+++ src/main/java/com/briix/briixtesting/BriixTestingApplication.java
@@ -0,0 +1,13 @@
+package com.briix.briixtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BriixTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BriixTestingApplication.class, args);
+	}
+
+}
